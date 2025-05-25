6335f7f325fec969188dfbf9bf443f9917816b62
h1ve2 <xy@h1v.cn>
2025-05-25 15:21:53 +0800

    support item option (#2)

--- src/imgsplit/imgsplit.ts
+++ src/imgsplit/imgsplit.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import yargs from 'yargs';
-import {imgsplit, ImgSplitItemOption, ImgSplitOption} from "@imgsplit/core";
+import {imgsplit, ItemOption, ImgSplitOption} from "@imgsplit/core";
 import {hideBin} from "yargs/helpers";
 import path from 'upath';
 import {existsSync} from "node:fs";
@@ -15,23 +15,21 @@ export type SplitOption = Partial<ImgSplitOption> & {
 
 
 const parser = yargs()
-    .usage('Usage:  imgsplit xxx.png 256 ./savepath/')
+    .usage('Usage:  imgsplit ./xxx.png 256 ./savepath/')
 
-    .example('imgsplit xxx.png -h 256 ./savepath/', 'by item height:256')
-    .example('imgsplit -i xxx.png -c 10 -o ./savepath/', 'by item count:10')
-    .example('imgsplit -i xxx.png -c 10 -o ./savepath/', 'by item count:10')
+    .example('imgsplit ./xxx.png -h 256 ./savepath/', 'by item height:256')
+    .example('imgsplit ./xxx.png -c 10 -o ./savepath/', 'by item count:10')
 
     .options({
-        i: {type: 'string', alias: 'input', description: 'source image'},
         o: {type: 'string', alias: 'output', description: 'dist path'},
-        h: {type: 'number', alias: 'item-height', description: 'item height'},
+        h: {type: 'number', alias: 'height', description: 'by height'},
         c: {type: 'number', alias: 'count', description: 'by count'},
-        m: {type: 'string', alias: 'items'},
-    });
+        m: {type: 'array', alias: 'items'},
+    })
+    .demandCommand(1);
 
 const spinner = ora({
     text: 'init',
-    suffixText:'...'
 });
 
 
@@ -41,19 +39,10 @@ const spinner = ora({
 
     let options: SplitOption = {};
 
-    if (arg.i) {
-        options.input = arg.i;
-    } else if (arg._) {
+    if (arg._) {
         options.input = arg._[0] as string;
     }
 
-    if (arg.h) {
-        options.height = arg.h;
-    } else if (arg._ && arg._.length >= 2) {
-        options.height = arg._[1] as number;
-    }else{
-        options.height = 256;
-    }
 
     if (arg.c) {
         options.count = arg.c;
@@ -61,29 +50,60 @@ const spinner = ora({
 
     if (arg.m) {
         options.items = [];
-        console.log(arg.m);
-        const itemArr: string[] = arg.m.split(';');
+        for (let i = 0; i < arg.m.length; i++) {
+            const itemObj: Partial<ItemOption> = {};
+            const itemStr = (arg.m[i] as string);
+            let item: string[];
+            if (/[,，]/ig.test(itemStr))
+                item = itemStr.split(/[,，]/ig);
+            else
+                item = [itemStr];
 
-        for (let i = 0; i < itemArr.length; i++) {
-            console.log(itemArr[i]);
-            // const item:number[] = arg.item[i] as number[];
-            // const itemOption:Partial<ImgSplitItemOption>={};
-            // if(item)
-            //
-            // options.items.push(itemOption as ImgSplitItemOption);
+            switch (item.length) {
+                case 1:
+                    itemObj.y = parseInt(item[0]);
+                    break;
+                case 2:
+                    itemObj.y = parseInt(item[0]);
+                    itemObj.height = parseInt(item[1]);
+                    break;
+                case 4:
+                    itemObj.x = parseInt(item[0]);
+                    itemObj.y = parseInt(item[1]);
+                    itemObj.width = parseInt(item[2]);
+                    itemObj.height = parseInt(item[3]);
+                    break;
+            }
+
+            options.items.push(itemObj as ItemOption);
         }
 
     }
-    if (!existsSync(options.input)) {
-        if (options.input) {
-            spinner.fail('file not exists!');
-        } else {
-            spinner.fail('input file is empty!');
-        }
+
+    if (arg.h) {
+        options.height = arg.h;
+    } else if (arg._ && arg._.length >= 2 && (typeof arg._[1]) === 'number') {
+        options.height = arg._[1] as number;
+    } else if (!arg.c && !arg.m) {
+        options.height = 256;
+    }
+
+
+    if (options.input) {
+        options.input = path.normalize(options.input);
+    } else {
+        spinner.fail('input file is empty!');
         return;
     }
 
-    options.input = path.normalize(options.input);
+    console.log(`Options:`)
+    console.log(`${JSON.stringify(options, null, 2)}`);
+
+    if (!existsSync(options.input)) {
+        spinner.fail(`file not exists! ---> ${options.input}`);
+        return;
+    }
+
 
     if (arg.o) {
         options.ouput = arg.o;
@@ -91,10 +111,8 @@ const spinner = ora({
         options.ouput = arg._[arg._.length - 1] as string;
     } else {
         const inputFileName = path.trimExt(path.basename(options.input));
-
         options.ouput = path.joinSafe(path.dirname(options.input), `${inputFileName}-splitted/`)
     }
-    console.debug(`options : ${JSON.stringify(options)}`);
 
     spinner.start('splitting');
 
@@ -104,12 +122,12 @@ const spinner = ora({
     spinner.text = 'saving';
     spinner.render();
 
-    await new Promise(res=>setTimeout(res, 100));
+    await new Promise(res => setTimeout(res, 100));
 
     const r = await saveFile(options.ouput, result);
     if (r.success) {
         spinner.succeed(`success!`)
-        console.log(`${path.join(path.normalize(process.cwd()),options.ouput)}`);
+        console.log(`${path.join(path.normalize(process.cwd()), options.ouput)}`);
     } else {
         spinner.fail('failed!')
         console.log(`${r.msg}`);
